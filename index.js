fc221868886b88a7cd026e48234824eb4f35ca62
nitesh-gupta-64 <123109031@nitkkr.ac.in>
2024-01-01 06:28:56 +0530

    Completed

--- index.js
+++ index.js
@@ -18,16 +18,24 @@ let passwordLength = 10;
 let checkCount = 0;
 handleSlider();
 //set Strength circle color to grey
+setIndicator("#ccc");
+
+handleCheckboxChange()
 
 function handleSlider() {
     inputSlider.value = passwordLength;
     lengthDisplay.innerText = passwordLength;
     //kuch to bachaa hai
+
+    const min = inputSlider.min;
+    const max = inputSlider.max;
+    inputSlider.style.backgroundSize = ( (passwordLength - min)*100/(max - min)) + "% 100%"
 }
 
 function setIndicator(color) {
     indicator.style.backgroundColor = color;
     //shadow
+    indicator.style.boxShadow = `0px 0px 12px 1px ${color}`;
 }
 
 function getRndInteger(min, max) {
